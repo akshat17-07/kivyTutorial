29116d1b5e76fcb7c7aa7fc0de33f3f45ffaa209
akshar17-07 <akshatharlalka.ah@icloud.com>
2021-09-14 02:07:30 -0400

    kivy3

---
+++ main.py
@@ -0,0 +1,13 @@
+import kivy
+from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
+
+class myLayout(FloatLayout):
+    pass
+
+class ButtonApp(App):
+    def build(self):
+        return muLayout()
+
+if __name__ == "__main__":
+    ButtonApp().run()
